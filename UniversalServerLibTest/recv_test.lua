8cb644441cd545466cbefbfded49b6f24a3ba6ee
Huan Xia <huxia1124@gmail.com>
2016-10-10 15:58:29 -0500

    Fix script bug : typo

--- UniversalServerLibTest/recv_test.lua
+++ UniversalServerLibTest/recv_test.lua
@@ -5,7 +5,7 @@ require 'test_add'
 	    if math.random(40000) > 39996 then
 		    --print("Received!! : TID = "..utils.GetThreadId())  
 			print("-----------------------------------")
-			print("Receive: "..math.floor((server.ReceiveBytesPerSecond/1024)).." KB/s,\tPackages: "..server.ReceiveCountPerSecond.." pkg/sec,\t Current clients: "..server:GetTcpClientCount())
+			print("Receive: "..math.floor((server.ReceivedBytesPerSecond/1024)).." KB/s,\tPackages: "..server.ReceivedCountPerSecond.." pkg/sec,\t Current clients: "..server:GetTcpClientCount())
 			print("         "..math.floor((server.TotalReceivedBytes/1024/1024)).." MB,\tPackages: "..server.TotalReceivedCount.." pkg")
 			print("Send:    "..math.floor((server.SentBytesPerSecond/1024)).." KB/s,\tPackages: "..server.SentCountPerSecond.." pkg/sec")
 			print("         "..math.floor((server.TotalSentBytes/1024/1024)).." MB,\tPackages: "..server.TotalSentCount.." pkg")
